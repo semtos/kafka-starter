e9d16c3cf7253cdf144c3fd1ac9fcc393eae0ad9
Semih Tosun <tsn.smh>
2020-05-02 01:14:38 +0300

    Producer-Consumer

---
+++ Consumer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Confluent.Kafka;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+
+        private static readonly string TOPIC = "LOOP-TOPIC";
+        private static readonly string NODES = "localhost:9092,localhost:9093,localhost:9094";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Consumer {args[0]}");
+            MessageProcessor();
+        }
+
+        private static void MessageProcessor()
+        {
+            //string consumerGroupId = "consumer-group-1";
+            string consumerGroupId = Guid.NewGuid().ToString();
+            var config = new ConsumerConfig()
+            {
+                GroupId = consumerGroupId,
+                BootstrapServers = NODES,
+                AutoOffsetReset = AutoOffsetReset.Latest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumerClient = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                try
+                {
+                    consumerClient.Subscribe(TOPIC);
+
+                    while (true)
+                    {
+                        var consumeResult = consumerClient.Consume(cts.Token);
+                        System.Console.WriteLine($"{consumeResult.TopicPartitionOffset} Processed message: {consumeResult.Value}");
+                        //commits the message log
+                        consumerClient.Commit(consumeResult);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                catch (KafkaException e)
+                {
+                    System.Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+                finally
+                {
+                    consumerClient.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Confluent.Kafka;
+using System;
+
+namespace Producer
+{
+    class Program
+    {
+
+        private static readonly string TOPIC = "LOOP-TOPIC";
+        private static readonly string NODES = "localhost:9092,localhost:9093,localhost:9094";
+
+        static void Main(string[] args)
+        {
+            System.Console.Title = "Producer";
+            for (int i = 0; i < 100; i++)
+            {
+                WriteMessagesAsync($"Test Message {i}").Wait();
+            }
+        }
+
+        private static async Task WriteMessagesAsync(string message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = NODES
+            };
+            using (var p = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync(TOPIC, new Message<string, string> { Key = Guid.NewGuid().ToString(), Value = message });
+                    System.Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (KafkaException e)
+                {
+                    System.Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
